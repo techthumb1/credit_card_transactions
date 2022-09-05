a09f5c7023ffdf47bee796934f4af7ede6a3ec90
Jason Robinson <robinsonjason761@gmail.com>
2022-09-04 23:41:28 -0500

    General commit

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,32 +1,108 @@
 # Path: webapp/app.py
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from sklearn.preprocessing import StandardScaler
 
-from flask import Flask, render_template, request, redirect, url_for, flash
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
+model = pickle.load(open('classification_model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-    
 
-# Get a predict route for the form
-@app.route('/predict', methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        # Get the data from the POST request.
-        data = request.form.to_dict()
-        print(data)
-        return redirect(url_for('index'))
 
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    final_features = scaler.transform(final_features)    
+    prediction = model.predict(final_features)
+    y_probabilities_test = model.predict_proba(final_features)
+    y_prob_success = y_probabilities_test[:, 1]
+    print("Final Features",final_features)
+    print("Prediction:",prediction)
+    output = round(prediction[0], 2)
+    y_prob=round(y_prob_success[0], 3)
+    print(output)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('index'))
-    return render_template('login.html')
+    if output == 1:
+        return render_template('layout.html', prediction_text='The transaction is fraudulent')
+    else:
+        return render_template('layout.html', prediction_text='The transaction is not fraudulent')
 
-if __name__ == '__main__':
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#from flask import Flask, render_template, request, redirect, url_for, flash
+#
+#app = Flask(__name__)
+#
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
+#    
+#
+## Get a predict route for the form
+#@app.route('/predict', methods=['POST'])
+#def predict():
+#    if request.method == 'POST':
+#        # Get the data from the POST request.
+#        data = request.form.to_dict()
+#        print(data)
+#        return redirect(url_for('index'))
+#
+#
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    if request.method == 'POST':
+#        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+#            error = 'Invalid Credentials. Please try again.'
+#        else:
+#            return redirect(url_for('index'))
+#    return render_template('login.html')
+#
+#if __name__ == '__main__':
+#    app.run(debug=True)
+#
--- WebApp/model.py
+++ WebApp/model.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import pickle
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import sklearn
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
@@ -14,9 +16,9 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import precision_recall_curve
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-import sys
 import matplotlib.pyplot as plt
 import lightgbm as lgb
+
 
 # Get the data
 cct1 = pd.read_csv('/Users/jasonrobinson/Documents/Data-Engineering-Credit-Card-Transactions/transactions.csv')
@@ -82,29 +84,22 @@ print("Classification Report: ", classification_report(y_test, y_pred))
 print("ROC AUC Score: ", roc_auc_score(y_test, y_pred_prob))
 print("Precision Recall Curve: ", precision_recall_curve(y_test, y_pred_prob))
 
-# Save the model
-pickle.dump(model, open('model.pkl', 'wb'))
+# Save the model to disk
+import pickle
+clf_model = 'classification_model.sav'
+pickle.dump(model, open(clf_model, 'wb'))
+
 
 # Load the model
-model = pickle.load(open('model.pkl', 'rb'))
+# Load the model from disk
+loaded_model = pickle.load(open(clf_model, 'rb'))
+result = loaded_model.score(X_test, y_test)
+print(result)
 
-# Predict
-print(model.predict([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165,
-166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332
-]]))
+# Return is fraud or not fraud
+y_pred = loaded_model.predict(X_test)
+print(y_pred)
 
-
-
-
-#print(model.get_accuracy())
-#print(model.get_confusion_matrix())
-#print(model.get_classification_report())
-#print(model.get_roc_auc_score())
-#print(model.get_precision_recall_curve())
-#model.save_model()
-## Load the model
-#model.load_model()
-#print(model.predict(X_test))
-#print(model.predict_proba(X_test))
-
-# Path: WebApp/app.py
+# Return probability of fraud
+y_pred_prob = loaded_model.predict_proba(X_test)[:,1]   
+print(y_pred_prob)
